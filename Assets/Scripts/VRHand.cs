d874311a86474bf432812e1cfb762c0986d4805f
shaderpopcorn <kwmail@gmx.de>
2020-11-04 21:10:07 +0100

    pick up

--- Assets/Scripts/VRHand.cs
+++ Assets/Scripts/VRHand.cs
@@ -32,6 +32,12 @@ public class VRHand: MonoBehaviour
 
     public Transform vrRig;
 
+    public float smoothnessValue = 0.2f;
+
+    public LayerMask layerMask;
+
+    public Transform heldObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,18 +49,25 @@ public class VRHand: MonoBehaviour
     void Update()
     {
         // IF the grip button of the proper hand is pressed run the close animation
-        if( Input.GetButtonDown( handness + "Grip" ) )
+        if (Input.GetButtonDown(handness + "Grip"))
         {
-            anim.SetBool( "GripPressed", true );
+            anim.SetBool("GripPressed", true);
 
-            // Pick up object
-            // Parent to holdPosition transform ref, zero out its transform and turn of gravity
-            hoveredObject.SetParent( holdPosition );
-            hoveredObject.GetComponent<Rigidbody>().useGravity = false;
-            hoveredObject.localPosition = Vector3.zero;
-            hoveredObject.localRotation = Quaternion.identity;
-
-            isHolding = true;
+            if (hoveredObject != null) {
+                // Pick up object
+                // Parent to holdPosition transform ref, zero out its transform and turn of gravity
+                GrabObject(hoveredObject);
+            }
+            else
+            {
+                Ray ray = new Ray(transform.position, transform.forward);
+                RaycastHit hitInfo = new RaycastHit();
+                if (Physics.Raycast(ray, out hitInfo, 100, layerMask))
+                {
+                   // Start coroutine
+                   StartCoroutine(SmoothMoveToHand(hitInfo.collider.transform));
+                }
+            }
         }
 
         // IF the grip button of the proper hand is released run the open animation
@@ -64,8 +77,8 @@ public class VRHand: MonoBehaviour
 
             // Drop an object here (if holding one)
             // Set the parent of the held object to empty and turn gravity back on
-            hoveredObject.SetParent( null );
-            hoveredObject.GetComponent<Rigidbody>().useGravity = true;
+            heldObject.SetParent( null );
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
 
             isHolding = false;
         }
@@ -78,7 +91,10 @@ public class VRHand: MonoBehaviour
             if (Physics.Raycast(ray, out hitInfo)) // if youo use the 'out' keyword, yoou must change the variable
             { 
                 teleportVisualRef.gameObject.SetActive( true );
-                teleportVisualRef.position = hitInfo.point;
+                Vector3 desiredPosition = hitInfo.point;
+                Vector3 vecToDesired = desiredPosition - teleportVisualRef.position;
+                vecToDesired *= smoothnessValue;
+                teleportVisualRef.position += vecToDesired;
                 if (Input.GetButtonDown( handness + "Trigger" ))
                 {
                     vrRig.position = new Vector3( hitInfo.point.x, vrRig.position.y, hitInfo.point.z );
@@ -89,6 +105,17 @@ public class VRHand: MonoBehaviour
                 teleportVisualRef.gameObject.SetActive( false );
             }
         }
+    }
+
+    private void GrabObject(Transform objectToGrab)
+    {
+        heldObject = objectToGrab;
+        heldObject.SetParent(holdPosition);
+        heldObject.GetComponent<Rigidbody>().useGravity = false;
+        heldObject.localPosition = Vector3.zero;
+        heldObject.localRotation = Quaternion.identity;
+
+        isHolding = true;
     }
 
     private void OnTriggerEnter( Collider other )
@@ -107,5 +134,35 @@ public class VRHand: MonoBehaviour
         {
             hoveredObject = null;
         }
+    }
+
+    private IEnumerator SmoothMoveToHand(Transform objectToMove)
+    {
+        float currentTime = 0;
+        Vector3 startPos = objectToMove.position; // we have to define it outside, so the 'objectToMove' is not moving
+        Vector3 endPos = holdPosition.position;
+        while (currentTime < 1)
+        {
+            // your own update loop
+            objectToMove.position = Vector3.Lerp(startPos, endPos, currentTime);
+
+            yield return null; // leave and come back in the next frame
+            
+            currentTime += Time.deltaTime; // linear
+            currentTime += Time.deltaTime + currentTime; // exponential
+        }
+        
+        GrabObject(objectToMove);
+    }
+
+    // this is the way to bring in other functions
+    public float Lerp(float a, float b, float t)
+    {
+        return ((b - a) * t) + a;
+    }
+
+    public Vector3 LerpV(Vector3 a, Vector3 b, float t)
+    {
+        return new Vector3( Lerp(a.x, b.x, t), Lerp(a.y, b.y, t), Lerp(a.z, b.z, t) );
     }
 }
